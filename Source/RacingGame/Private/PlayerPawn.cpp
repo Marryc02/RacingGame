e82cc7035d6770c163673359195bf808f69b59c0
Markus Rychter <89453088+Marryc02@users.noreply.github.com>
2022-04-23 21:25:47 +0200

    Andreas is smooth brained
    ligma

---
+++ Source/RacingGame/Private/PlayerPawn.cpp
@@ -0,0 +1,170 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlayerPawn.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	SetRootComponent(CollisionBox);
+
+	PlayerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlayerMesh"));
+	PlayerMesh->SetupAttachment(GetRootComponent());
+
+	FloatingPawnMovementComp = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("PawnMovementComp"));
+	FloatingPawnMovementComp->Deceleration = 2000;
+	FloatingPawnMovementComp->Acceleration = 2000;
+
+
+	// Camera 1
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->SetUsingAbsoluteRotation(false);
+
+	SpringArm->SetRelativeRotation(FRotator(-25.f, 0.f, 0.f));
+	SpringArm->TargetArmLength = 600.f;
+	SpringArm->bEnableCameraLag = false;
+	SpringArm->CameraLagSpeed = 5.f;
+
+	SpringArm->SetupAttachment(PlayerMesh);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->bUsePawnControlRotation = false;
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	
+
+	// Camera 2
+
+	SpringArm2 = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp2"));
+	SpringArm2->bDoCollisionTest = false;
+	SpringArm2->SetUsingAbsoluteRotation(false);
+
+	SpringArm2->SetRelativeRotation(FRotator(0.f, 0.f, 0.f));
+	SpringArm2->TargetArmLength = -100.f;
+	SpringArm2->bEnableCameraLag = false;
+	SpringArm2->CameraLagSpeed = 5.f;
+
+	SpringArm2->SetupAttachment(PlayerMesh);
+
+	Camera2 = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera2"));
+	Camera2->bUsePawnControlRotation = false;
+	Camera2->SetupAttachment(SpringArm2, USpringArmComponent::SocketName);
+	Camera2->AddLocalOffset(FVector(0.0f, 0.0f, 50.0f));
+
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+	AddMovementInput(GetActorForwardVector(), 1.f);
+
+	
+	if (BoostActivated == true)
+	{
+		BoostDuration += DeltaTime;
+		if (BoostDuration > BoostLimit)
+		{
+			BoostActivated = false;
+			FloatingPawnMovementComp->MaxSpeed = 1000.0f;
+			BoostDuration = 0.f;
+		}
+	}
+	
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	check(PlayerInputComponent != nullptr);
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("PauseGame", EInputEvent::IE_Pressed, this, &APlayerPawn::PauseGame).bExecuteWhenPaused = true;
+	PlayerInputComponent->BindAction("SwitchPerspective", EInputEvent::IE_Pressed, this, &APlayerPawn::SwitchPerspective);
+
+	PlayerInputComponent->BindAction("AirBrakes", EInputEvent::IE_Pressed, this, &APlayerPawn::StartAirBrakes);
+	PlayerInputComponent->BindAction("AirBrakes", EInputEvent::IE_Released, this, &APlayerPawn::StopAirBrakes);
+	PlayerInputComponent->BindAxis("MoveLeftRight", this, &APlayerPawn::MoveLeftRight);
+	PlayerInputComponent->BindAxis("MoveUpDown", this, &APlayerPawn::MoveUpDown);
+
+	PlayerInputComponent->BindAction("BoostPower", EInputEvent::IE_Pressed, this, &APlayerPawn::BoostActivation);
+
+}
+
+void APlayerPawn::PauseGame() {
+	if (isPaused == false)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PAUSED"));
+		GEngine->AddOnScreenDebugMessage(-1, 12.f, FColor::Red, FString::Printf(TEXT("You have paused the game!")));
+		UGameplayStatics::SetGamePaused(GetWorld(), true);
+		isPaused = true;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UNPAUSED"));
+		GEngine->AddOnScreenDebugMessage(-1, 12.f, FColor::Yellow, FString::Printf(TEXT("You have unpaused the game!")));
+		UGameplayStatics::SetGamePaused(GetWorld(), false);
+		isPaused = false;
+	}
+}
+
+void APlayerPawn::SwitchPerspective() {
+	UE_LOG(LogTemp, Warning, TEXT("Switching perspective view"));
+
+		if (firstPerson == false) {
+			Camera->Deactivate();
+			Camera2->Activate();
+
+			firstPerson = true;
+		}
+		else {
+			Camera->Activate();
+			Camera2->Deactivate();
+
+			firstPerson = false;
+		}
+}
+
+void APlayerPawn::BoostActivation() {
+	if (BoostActivated == false)
+	{
+		FloatingPawnMovementComp->MaxSpeed = 5000.0f;
+		BoostActivated = true;
+	}
+}
+
+void APlayerPawn::StartAirBrakes() {
+	FloatingPawnMovementComp->MaxSpeed = 500.0f;
+}
+
+void APlayerPawn::StopAirBrakes() {
+	FloatingPawnMovementComp->MaxSpeed = 1000.0f;
+}
+
+void APlayerPawn::MoveLeftRight(float Value) {
+	LeftRightTurnSpeed = FMath::FInterpTo(LeftRightTurnSpeed, Value, GetWorld()->GetDeltaSeconds(), 1.f);
+	AddActorLocalRotation(FRotator(0.f, 1.f, 0.f) * LeftRightTurnSpeed);
+	PlayerMesh->USceneComponent::SetRelativeRotation(FRotator(0.f, 0.f, 40.f) * LeftRightTurnSpeed);
+}
+
+void APlayerPawn::MoveUpDown(float Value) {
+	UpDownTurnSpeed = FMath::FInterpTo(UpDownTurnSpeed, Value, GetWorld()->GetDeltaSeconds(), 1.f);
+	AddActorLocalRotation(FRotator(1.f, 0.f, 0.f) * UpDownTurnSpeed);
+}
---
+++ Source/RacingGame/Public/PlayerPawn.h
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Kismet/GameplayStatics.h"
+#include "PlayerPawn.generated.h"
+
+UCLASS()
+class RACINGGAME_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	// Other
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerMesh")
+		class UShapeComponent* CollisionBox = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerMesh")
+		class UStaticMeshComponent* PlayerMesh = nullptr;
+
+
+	// Cameras
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cameras")
+		class USpringArmComponent* SpringArm {
+		nullptr
+	};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cameras")
+		class UCameraComponent* Camera = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cameras")
+		class USpringArmComponent* SpringArm2 {
+		nullptr
+	};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cameras")
+		class UCameraComponent* Camera2 = nullptr;
+
+
+	// Movement
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		class UFloatingPawnMovement* FloatingPawnMovementComp = nullptr;
+
+
+	UPROPERTY(EditAnywhere, Category = "BoostTime")
+		float BoostLimit = 5.f;
+
+	UPROPERTY(EditAnywhere, Category = "BoostTime")
+		float BoostDuration = 0.f;
+
+private:
+
+	bool isPaused = false;
+	void PauseGame();
+
+	bool firstPerson = false;
+	void SwitchPerspective();
+
+	float LeftRightTurnSpeed = 0.f;
+	float UpDownTurnSpeed = 0.f;
+
+	void StartAirBrakes();
+	void StopAirBrakes();
+	void MoveLeftRight(float Value);
+	void MoveUpDown(float value);
+
+	bool BoostActivated = false;
+	void BoostActivation();
+
+};
